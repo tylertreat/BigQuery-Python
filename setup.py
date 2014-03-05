d43bcc978b1d79a20820ab1df73bd69d5d3c100d
Tyler Treat <tyler.treat@webfilings.com>
2014-03-05 16:37:14 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+setup_args = dict(
+    name='BigQuery-Python',
+    description='Simple Python client for interacting with Google BigQuery.',
+    url='https://github.com/tylertreat/BigQuery-Python',
+    version=VERSION,
+    license='Apache',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['google-api-python-client', 'pyopenssl'],
+    author='Tyler Treat',
+    author_email='ttreat31@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
+
